18f9637df8ca6a0e8b4e1d372377280fafee614a
Shuki Shukrun <shukrun.shuki@gmail.com>
2013-10-23 11:41:47 +0300

    itegration of image upload to tinymce.

--- jset/jquery/jquery.jset.components.js
+++ jset/jquery/jquery.jset.components.js
@@ -918,13 +918,13 @@
 			script_url : $.jset.defaults.dir_pre + 'jset/widget/tinymce/jscripts/tiny_mce/tiny_mce.js',
 			// General options
 			theme : 'advanced',
-			plugins : 'safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template',
-			
+			plugins : 'jbimages,safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template',
+			language : 'en',
 			// Theme options
 			theme_advanced_buttons1 : 'newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect',
 			theme_advanced_buttons2 : 'cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor',
 			theme_advanced_buttons3 : 'tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen',
-			theme_advanced_buttons4 : 'insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak',
+			theme_advanced_buttons4 : 'jbimages,|,insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak',
 			theme_advanced_toolbar_location : 'top',
 			theme_advanced_toolbar_align : 'left',
 			theme_advanced_statusbar_location : 'bottom',
@@ -942,6 +942,8 @@
 				username : 'Some User',
 				staffid : '991234'
 			},
+			
+			relative_urls : false,
 			//for removing the <p></p> inital tag
 			//force_p_newlines : false,
 			//force_br_newlines : false,
