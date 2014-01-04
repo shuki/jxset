b99281f0daad2730bc4c35c42bd2212f5e5d44e8
Shuki Shukrun <shukrun.shuki@gmail.com>
2014-01-04 11:38:49 +0200

    get_user_name implementation

--- jset/server/class/general.class.php
+++ jset/server/class/general.class.php
@@ -5,11 +5,14 @@ class general
 	public static function get_export_limit()
 	{
 		return config::no_export_limit;
-		//return $_SESSION['roles'] !== '' && ($_SESSION['roles'] <= 20 || $_SESSION['user_id'] == 247899) ? config::no_export_limit : config::export_limit;
 	}
 	
 	public static function get_user_id()
 	{
 		return 200000;
+	}
+	
+	public static function get_user_name(){
+		return 'user_name: ' . $_SERVER['PHP_AUTH_USER'];
 	}
 }
