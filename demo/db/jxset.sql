c18e1cafd81d0c30aba402e7c0137fc44419c4ef
Shuki Shukrun <shukrun.shuki@gmail.com>
2014-05-13 01:21:23 +0300

    update jxset.sql

--- demo/db/jxset.sql
+++ demo/db/jxset.sql
@@ -10,6 +10,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping database structure for jxset
+CREATE DATABASE IF NOT EXISTS `jxset` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `jxset`;
+
+
 -- Dumping structure for table jxset.demo
 DROP TABLE IF EXISTS `demo`;
 CREATE TABLE IF NOT EXISTS `demo` (
