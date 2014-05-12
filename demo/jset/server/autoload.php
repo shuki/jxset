1330b49ec19f43fe59b5fa8ffe6b0a7d9d1d8908
Shuki Shukrun <shukrun.shuki@gmail.com>
2014-05-13 01:38:42 +0300

    set error handling to not display notice, strict and warning errors.

--- demo/jset/server/autoload.php
+++ demo/jset/server/autoload.php
@@ -6,7 +6,7 @@ include_once(RELATIVE_PATH_TO_ROOT . JSET_SERVER_CLASS_PATH . "config.class.php"
 ini_set("log_errors" , "1");
 ini_set("error_log" , RELATIVE_PATH_TO_ROOT . config::errorLogFile);
 ini_set("display_errors" , "1"); // set to 0 in production
-ini_set('error_reporting', E_ALL ^ E_NOTICE);
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
 
 function __autoload($class_name) {
 	if (is_file(RELATIVE_PATH_TO_ROOT . JSET_SERVER_CLASS_PATH . $class_name . '.class.php'))
