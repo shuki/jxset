4d22fc1eaf02379ce9113e5e2d19235ea03d3c8e
Shuki Shukrun <shukrun.shuki@gmail.com>
2014-05-13 18:15:24 +0300

    tidy error handling visability

--- jset/server/autoload.php
+++ jset/server/autoload.php
@@ -4,7 +4,6 @@ include_once("class/jset_autoload.class.php");
 ini_set("log_errors" , "1");
 ini_set("error_log" , jset_autoload::path('../../..', '') . config::errorLogFile);
 ini_set("display_errors" , "1"); // set to 0 in production
-//ini_set('error_reporting', E_ALL ^ E_NOTICE);
 ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING);
 
 function __autoload($class_name) {
