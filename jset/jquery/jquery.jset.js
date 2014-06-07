9f70420af0e87e2baa672cabdaeac0fa58622497
Shuki Shukrun <shukrun.shuki@gmail.com>
2014-06-07 11:38:26 +0300

    fix toolbar filter selector.

--- jset/jquery/jquery.jset.js
+++ jset/jquery/jquery.jset.js
@@ -1032,7 +1032,8 @@
 		},
 		
 		get_filterToolbar_field: function(grid, field_name){
-			var exclude = $("div.ui-jqgrid[id^='gbox_'] .FormElement, .ui-search-input .FormElement", $.jset.fn.get_grid_container(grid));
+			//var exclude = $("div.ui-jqgrid[id^='gbox_'] .FormElement, .ui-search-input .FormElement", $.jset.fn.get_grid_container(grid));
+			var exclude = $("div.ui-jqgrid[id^='gbox_'] .FormElement", $.jset.fn.get_grid_container(grid));
 			return $(':input[id=gs_' + field_name + ']', $.jset.fn.get_grid_container(grid)).not(exclude);
 		},
 		
