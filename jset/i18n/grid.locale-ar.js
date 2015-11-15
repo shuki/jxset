fb4f6da5bebf0131d88f9eda3f3b521d84cc2552
Shuki Shukrun <shukrun.shuki@gmail.com>
2015-11-15 12:59:59 +0200

    get right arabic grid translation.

--- jset/i18n/grid.locale-ar.js
+++ jset/i18n/grid.locale-ar.js
@@ -1,3 +1,4 @@
+;(function($){
 /**
  * jqGrid Arabic Translation
  * 
@@ -6,46 +7,13 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
 **/
-/*global jQuery, define */
-(function( factory ) {
-	"use strict";
-	if ( typeof define === "function" && define.amd ) {
-		// AMD. Register as an anonymous module.
-		define([
-			"jquery",
-			"../grid.base"
-		], factory );
-	} else {
-		// Browser globals
-		factory( jQuery );
-	}
-}(function( $ ) {
-
 $.jgrid = $.jgrid || {};
-if(!$.jgrid.hasOwnProperty("regional")) {
-	$.jgrid.regional = [];
-}
-$.jgrid.regional["ar"] = {
+$.extend($.jgrid,{
 	defaults : {
 		recordtext: "تسجيل {0} - {1} على {2}",
 		emptyrecords: "لا يوجد تسجيل",
 		loadtext: "تحميل...",
-		savetext: "Saving...",
-		pgtext : "صفحة {0} على {1}",
-		pgfirst : "First Page",
-		pglast : "Last Page",
-		pgnext : "Next Page",
-		pgprev : "Previous Page",
-		pgrecs : "Records per Page",
-		showhide: "Toggle Expand Collapse Grid",
-		// mobile
-		pagerCaption : "Grid::Page Settings",
-		pageText : "Page:",
-		recordPage : "Records per Page",
-		nomorerecs : "No more records...",
-		scrollPullup: "Pull up to load more...",
-		scrollPulldown : "Pull down to refresh...",
-		scrollRefresh : "Release to refresh..."
+		pgtext : "صفحة {0} على {1}"
 	},
 	search : {
 		caption: "بحث...",
@@ -104,12 +72,7 @@ $.jgrid.regional["ar"] = {
 		alertcap: "تحذير",
 		alerttext: "يرجى إختيار السطر",
 		viewtext: "",
-		viewtitle: "إظهار السطر المختار",
-		savetext: "",
-		savetitle: "Save row",
-		canceltext: "",
-		canceltitle : "Cancel row editing",
-		selectcaption : "Actions..."
+		viewtitle: "إظهار السطر المختار"
 	},
 	col : {
 		caption: "إظهار/إخفاء الأعمدة",
@@ -139,7 +102,7 @@ $.jgrid.regional["ar"] = {
 			S: function (j) {return j == 1 ? 'er' : 'e';},
 			srcformat: 'Y-m-d',
 			newformat: 'd/m/Y',
-			parseRe : /[#%\\\/:_;.,\t\s-]/,
+			parseRe : /[Tt\\\/:_;.,\t\s-]/,
 			masks : {
 				ISO8601Long:"Y-m-d H:i:s",
 				ISO8601Short:"Y-m-d",
@@ -153,8 +116,7 @@ $.jgrid.regional["ar"] = {
 				UniversalSortableDateTime: "Y-m-d H:i:sO",
 				YearMonth: "F, Y"
 			},
-			reformatAfterEdit : false,
-			userLocalTime : false
+			reformatAfterEdit : false
 		},
 		baseLinkUrl: '',
 		showAction: '',
@@ -162,5 +124,5 @@ $.jgrid.regional["ar"] = {
 		checkbox : {disabled:true},
 		idName : 'id'
 	}
-};
-}));
+});
+})(jQuery);
