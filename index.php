f91d483ad95ba9bb32082b6af723fdc3885273e4
Shuki Shukrun <shukrun.shuki@gmail.com>
2017-06-06 13:02:48 +1200

    proper html and smile.

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
-<?php 
-/*
- * jxset
- * Copyright (c) 2010 - 2013, Shuki Shukrun (shukrun.shuki at gmail.com).
- * Dual licensed under the MIT and GPL licenses
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- */
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
+<link rel="shortcut icon" href="jset/img/smile.gif" type="image/x-icon" />
 <title>Jxset - Custom Information Systems</title>
 </head>
 <body>
-		<div style="width:100%; margin-top:10px; text-align: center">
-			<img src="jset/img/jxset.jpg"/>
-			<div><h2>Contact: <a href="mailto:shukrun.shuki@gmail.com?Subject=Jxset" target="_blank">shukrun.shuki@gmail.com</a></h2></div>
+	<div style="width:100%; margin-top:10px; text-align: center">
+	<img src="jset/img/jxset.jpg"/>
+	<div><h2>Contact: <a href="mailto:shukrun.shuki@gmail.com?Subject=Jxset" target="_blank">shukrun.shuki@gmail.com</a></h2></div>
 </body>
 </html>
